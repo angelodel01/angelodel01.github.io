7ffb955e73d0d648373212d823d7f54ec3c78811
angelodel01 <angeldelaur@gmail.com>
2018-06-27 12:06:17 -0700

    Update source.js

--- source.js
+++ source.js
@@ -10,10 +10,25 @@ function protectedClick(){
 		protectedContent();
 	} else{
 		window.location = "https://cognito-dev.calpoly.edu/login?response_type=token&client_id=2fior6770hvto4u6kuq084j7fu&redirect_uri=https://angelodel01.github.io";
+		listener();
 	}
 	removeTitle();
 	return;
 }
+
+function listener(){
+	var xhr = new XMLHttpRequest();
+	xhr.addEventListener('load', function(){
+		var responseObject = JSON.parse(this.response);
+		console.log(responseObject);
+		if (responseObject.token){
+			alert(responseObject.token);
+		}else{
+			alert("no response received");
+		}
+	})
+}
+
 
 function getCookie(cname) {
     var name = cname + "=";

ea6841dc9af48644cd2da2df5fbc36635ff38377
angelodel01 <angeldelaur@gmail.com>
2018-06-29 14:16:40 -0700

    Update source.js

--- source.js
+++ source.js
@@ -290,7 +290,7 @@ function protectedContent(id_token){
 	myHeaders.append("Access-Control-Allow-Origin", "http://angelodel01.github.io/");
 
 	createParagraph("display");
-	fetch(url, {myHeaders}).then(function(response){
+	fetch(url, {myHeaders, mode : "cors"}).then(function(response){
 		return response.json();	
 		})
 		.then(function(myJson){

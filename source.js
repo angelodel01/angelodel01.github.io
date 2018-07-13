68553220b9bbc3c4805db876af014b9bcac2bc90
angelodel01 <angeldelaur@gmail.com>
2018-07-13 09:37:52 -0700

    Debuggin router

--- source.js
+++ source.js
@@ -25,44 +25,45 @@ console.log("LOADING PAGE");
 
 
 
-// ;(function(window) {
-//
-//   // exit if the browser implements that event
-//   if ("onhashchange" in window) { return; }
-//
-//   var location = window.location,
-//     oldURL = location.href,
-//     oldHash = location.hash;
-//
-//   // check the location hash on a 100ms interval
-//   setInterval(function() {
-//     var newURL = location.href,
-//       newHash = location.hash;
-//
-//     // if the hash has changed and a handler has been bound...
-//     if (newHash != oldHash && typeof window.onhashchange === "function") {
-//       // execute the handler
-//       window.onhashchange({
-//         type: "hashchange",
-//         oldURL: oldURL,
-//         newURL: newURL
-//       });
-//
-//       oldURL = newURL;
-//       oldHash = newHash;
-//     }
-//  }, 100);
-//
-// })(window);
-//
-//
-//
-// window.onhashchange = function(jsonResp) {
-// 	console.log("new hash ", jsonResp.newURL)
-// 	console.log("old hash ", jsonResp.oldURL)
-// console.log("HASH changed", location.hash.substring(1));
-// 	render({page : location.hash.substring(1)})
-// }
+;(function(window) {
+
+  // exit if the browser implements that event
+  if ("onhashchange" in window) { return; }
+
+  var location = window.location,
+    oldURL = location.href,
+    oldHash = location.hash;
+
+  // check the location hash on a 100ms interval
+  setInterval(function() {
+    var newURL = location.href,
+      newHash = location.hash;
+
+    // if the hash has changed and a handler has been bound...
+    if (newHash != oldHash && typeof window.onhashchange === "function") {
+      // execute the handler
+      window.onhashchange({
+        type: "hashchange",
+        oldURL: oldURL,
+        newURL: newURL
+      });
+
+      oldURL = newURL;
+      oldHash = newHash;
+    }
+ }, 1000);
+
+})(window);
+
+
+
+window.onhashchange = function(jsonResp) {
+	console.log("new hash ", jsonResp.newURL)
+	console.log("old hash ", jsonResp.oldURL)
+console.log("HASH changed", location.hash.substring(1));
+	window.location = jsonResp.newURL
+	render({page : location.hash.substring(1)}, true)
+}
 ///////////////////////////////History popState
 
 window.addEventListener('popstate', function (event) {

e521b16c9a90f38447dab1175e25cd50400f635e
angelodel01 <angeldelaur@gmail.com>
2018-07-18 14:16:45 -0700

    cdn debug pt 3

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,75 +1,93 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Link, Redirect, withRouter} from 'react-router-dom'
+// import React from "react";
+// import { BrowserRouter as Router, Route, Link, Redirect, withRouter} from 'react-router-dom'
+//
+// import { searchFunction } from './stock.js'
+// import { accessFunction } from './repo.js'
+// import { personSearch } from './personSearch.js'
 
-import { searchFunction } from './stock.js'
-import { accessFunction } from './repo.js'
-import { personSearch } from './personSearch.js'
 
+const App = React.createClass({
+  render: function()  {
+     return (
+        <div>
+        <header>Cal Poly</header><br></br>
+        <Router>
+          <div id ="ogB">
 
-const App = () => (
-  <div>
-     <header>Cal Poly</header><br></br>
-     <Router>
-       <div id ="ogB">
+            <hr />
 
-         <hr />
-
-         <Route exact path="/" component={Home} />
-         <Route path="/stock" component={Stock} />
-         <Route path="/repo" component={Repo} />
-         <Route path="/protected" component={Protected} />
-         <Route path="/personSearch" component={PersonSearch} />
+            <Route exact path="/" component={Home} />
+            <Route path="/stock" component={Stock} />
+            <Route path="/repo" component={Repo} />
+            <Route path="/protected" component={Protected} />
+            <Route path="/personSearch" component={PersonSearch} />
+          </div>
+        </Router>
        </div>
-     </Router>
-    </div>
-);
+    )}
+});
 
-const Home = () => (
-  <div>
-    <h2 id= "title">HTML Buttons</h2>
-      <Link to="/repo"><button className= "button">Display Repos</button></Link>
-      <Link to="/stock"><button className= "button">Check Stock Info</button></Link>
-      <Link to="/protected"><button className= "button">Protected Resource</button></Link>
-      <Link to="/personSearch"><button className= "button">Search Directory</button></Link>
-  </div>
-);
+const Home = React.createClass({
+  render: function()  {
+     return (
+        <div>
+          <h2 id= "title">HTML Buttons</h2>
+            <Link to="/repo"><button className= "button">Display Repos</button></Link>
+            <Link to="/stock"><button className= "button">Check Stock Info</button></Link>
+            <Link to="/protected"><button className= "button">Protected Resource</button></Link>
+            <Link to="/personSearch"><button className= "button">Search Directory</button></Link>
+        </div>
+     )}
+});
 
-const Stock = () => (
-   <div id="contentItems" className="text">
-      <input type="text" placeholder="Type here..." id="Input" className="textBox"></input>
-      <button id="bn" onClick={searchFunction} className="button">Search Stock</button>
-      <Link to="/"><button className= "button">Home</button></Link>
-  </div>
-);
+const Stock = React.createClass({
+  render: function()  {
+     return (
+         <div id="contentItems" className="text">
+            <input type="text" placeholder="Type here..." id="Input" className="textBox"></input>
+            <button id="bn" onClick={searchFunction} className="button">Search Stock</button>
+            <Link to="/"><button className= "button">Home</button></Link>
+        </div>
+     })
+});
 
-const Repo = () => (
-  <div>
-      <div id="contentItems" className="text">
-         <input type="text" placeholder="Type here..." id="Input" className="textBox"></input>
-         <button id="bn" onClick={accessFunction} className="button">List Repos</button>
-      </div>
-    <Link to="/"><button className= "button">Home</button></Link>
-  </div>
-);
+const Repo = React.createClass({
+  render: function()  {
+     return (
+        <div>
+            <div id="contentItems" className="text">
+               <input type="text" placeholder="Type here..." id="Input" className="textBox"></input>
+               <button id="bn" onClick={accessFunction} className="button">List Repos</button>
+            </div>
+          <Link to="/"><button className= "button">Home</button></Link>
+        </div>
+     })
+});
 
-const PersonSearch = () => (
-  <div>
-      <div id="contentItems" className="text">
-         <input type="text" placeholder="Type here..." id="searchParam" className="textBox"></input>
-         <button id="bn" onClick={personSearch} className="button">Search Person</button>
-      </div>
-    <Link to="/"><button className= "button">Home</button></Link>
-  </div>
-);
+const PersonSearch = React.createClass({
+  render: function()  {
+     return (
+        <div>
+            <div id="contentItems" className="text">
+               <input type="text" placeholder="Type here..." id="searchParam" className="textBox"></input>
+               <button id="bn" onClick={personSearch} className="button">Search Person</button>
+            </div>
+          <Link to="/"><button className= "button">Home</button></Link>
+        </div>
+     })
+});
 
-const Protected = () => (
-  <div>
-    <Link to="/"><button className= "button">Home</button></Link>
-  </div>
-);
+const Protected = React.createClass({
+  render: function()  {
+     return (
+        <div>
+          <Link to="/"><button className= "button">Home</button></Link>
+        </div>
+     })
+});
 
-
-export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
+// export default App;
 
 
 
